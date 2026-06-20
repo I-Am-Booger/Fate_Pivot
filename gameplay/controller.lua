82821b8aea2fc38493f5a0b3251dfd732218bbdd
Gant Allen <15658332+I-Am-Booger@users.noreply.github.com>
2026-06-20 13:26:47 -0600

    Player can choose a target

--- gameplay/controller.lua
+++ gameplay/controller.lua
@@ -1,12 +1,32 @@
 --[[
-      1.controls 
-        A. The r buton on the keyboards calles the destroy_player() 
-           from spawning_player.lua
+      1.controls for the game
 ]]
-           
-function love.keypressed(key)
-    if key == "r" then 
-        destroy_player()
+ 
+function move_selector(move)
+
+    local targetX = currentPlayerCell.x
+    local targetY = currentPlayerCell.y
+
+    if move == "w" then targetY = currentPlayerCell.y - cell end 
+    if move == "s" then targetY = currentPlayerCell.y + cell end 
+    if move == "a" then targetX = currentPlayerCell.x - cell end 
+    if move == "d" then targetX = currentPlayerCell.x + cell end 
+
+    for i, cellData in ipairs(grid_table) do
+        if cellData.x == targetX and cellData.y == targetY then
+            selectedCell = cellData
+            return             
+        end 
     end
-    
 end 
+
+
+function love.keypressed(key)
+    if key == "w" then move_selector("w") end
+    if key == "s" then move_selector("s") end 
+    if key == "a" then move_selector("a") end 
+    if key == "d" then move_selector("d") end 
+
+    if key == "r" then destroy_player() end    
+end 
+
